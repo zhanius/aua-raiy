de1c17eaff6167a01a4ced82e30eb4922174ceec
zhansik <zhansulushakhizada@gmail.com>
2024-07-14 14:45:35 +0500

    last upd

--- components/search.vue
+++ components/search.vue
@@ -3,38 +3,32 @@
     <div class="flex max-w-2xl w-full justify-between mb-8">
       <h2 class="text-4xl sm:xl md:xl lg:5xl">Auaraiy.kz</h2>
       <a class="float-left w-10" target="_blank" href="https://t.me/zhansuuw">
-        <img src="https://img.icons8.com/?size=64&id=HTBOiI9JKlWY&format=png" alt="">
+        <img src="https://img.icons8.com/?size=64&id=HTBOiI9JKlWY&format=png" alt="" />
       </a>
     </div>
-    <div class="bg-orange-50 border-black border-2 p-2.5 outline-none shadow-[5px_5px_0px_rgba(0,0,0,1)] max-w-2xl w-full sm:px-10 sm:py-6 flex flex-col justify-center gap-6 items-start">
+    <div
+      class="bg-orange-50 border-black border-2 p-2.5 outline-none shadow-[5px_5px_0px_rgba(0,0,0,1)] max-w-2xl w-full sm:px-10 sm:py-6 flex flex-col justify-center gap-6 items-start">
       <div class="input-div w-full flex gap-2 sm:gap-3">
-        <input
-          id="search"
-          v-model="searchQuery"
+        <input id="search" v-model="searchQuery"
           class="flex-grow text-lg sm:text-xl md:text-2xl text-black border-black border-2 p-2.5 outline-none shadow-[5px_5px_0px_rgba(0,0,0,1)] focus:bg-[#f3d6f0] active:shadow-[2px_2px_0px_rgba(0,0,0,1)]"
-          placeholder="Enter Your Location"
-        />
-        <button @click="searchCity" class="bg-slate-300 border-black border-2 p-2.5 outline-none shadow-[5px_5px_0px_rgba(0,0,0,1)] active:shadow-[2px_2px_0px_rgba(0,0,0,1)] flex items-center justify-center" type="button">
+          placeholder="Enter Your Location" />
+        <button @click="searchCity"
+          class="bg-slate-300 border-black border-2 p-2.5 outline-none shadow-[5px_5px_0px_rgba(0,0,0,1)] active:shadow-[2px_2px_0px_rgba(0,0,0,1)] flex items-center justify-center"
+          type="button">
           <img src="../assets/magnifying-glass.png" alt="Search" class="w-6 h-6" />
         </button>
       </div>
       <p v-if="errorMessage" class="text-red-500">{{ errorMessage }}</p>
       <div class="w-full" v-if="weatherVisible">
-        <Weather 
-          :city="weatherData.city" 
-          :temperature="weatherData.temperature" 
-          :description="weatherData.description" 
-          :humidity="weatherData.humidity" 
-          :icon="weatherData.icon"
-          :wind="weatherData.wind"
-        />
+        <Weather :city="weatherData.city" :temperature="weatherData.temperature" :description="weatherData.description"
+          :humidity="weatherData.humidity" :icon="weatherData.icon" :wind="weatherData.wind" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Weather from './weather.vue';
+import Weather from "./weather.vue";
 
 export default {
   components: {
@@ -42,48 +36,71 @@ export default {
   },
   data() {
     return {
-      searchQuery: '',
+      searchQuery: "",
       weatherData: null,
       weatherVisible: false,
-      errorMessage: '',
+      errorMessage: "",
     };
   },
   methods: {
     translateDescription(description) {
       const translations = {
-        '01d': 'ashyq aspan',
-        '01n': 'ashyq aspan',
-        '02d': 'säl būltty aua raiy',
-        '02n': 'säl būltty aya raiy',
-        '03d': 'ūşpa būlttar',
-        '03n': 'ūşpa būlttar',
-        '04d': 'būltty aua raiy',
-        '04n': 'būltty aua raiy',
-        '09d': 'nöserli jañbyr',
-        '09n': 'nöserli jañbyr',
-        '10d': 'jauyn-şaşyn',
-        '10n': 'jauyn-şaşyn',
-        '11d': 'naizağai',
-        '11n': 'naizağai',
-        '13d': 'qar',
-        '13n': 'qar',
-        '50d': 'tūman',
-        '50n': 'tūman',
+        "01d": "ashyq aspan",
+        "01n": "ashyq aspan",
+        "02d": "säl būltty aua raiy",
+        "02n": "säl būltty aya raiy",
+        "03d": "ūşpa būlttar",
+        "03n": "ūşpa būlttar",
+        "04d": "būltty aua raiy",
+        "04n": "būltty aua raiy",
+        "09d": "nöserli jañbyr",
+        "09n": "nöserli jañbyr",
+        "10d": "jauyn-şaşyn",
+        "10n": "jauyn-şaşyn",
+        "11d": "naizağai",
+        "11n": "naizağai",
+        "13d": "qar",
+        "13n": "qar",
+        "50d": "tūman",
+        "50n": "tūman",
       };
 
       return translations[description] || description;
     },
+    getIconUrl(description) {
+      const translations = {
+        "01d": "https://img.icons8.com/?size=48&id=M0RPnzNPMiZi&format=png",
+        "01n": "https://img.icons8.com/?size=48&id=fehaCOqehQKm&format=png",
+        "02d": "https://img.icons8.com/?size=48&id=R5QLDwYPNm1R&format=png",
+        "02n": "https://img.icons8.com/?size=48&id=Al5kevhvRLMt&format=png",
+        "03d": "https://img.icons8.com/?size=48&id=W1YHvCxiIHyN&format=png",
+        "03n": "https://img.icons8.com/?size=48&id=W1YHvCxiIHyN&format=png",
+        "04d": "https://img.icons8.com/?size=48&id=YCGDxvKcUXYv&format=png",
+        "04n": "https://img.icons8.com/?size=48&id=YCGDxvKcUXYv&format=png",
+        "09d": "https://img.icons8.com/?size=48&id=95scIGHYeKzT&format=png",
+        "09n": "https://img.icons8.com/?size=48&id=95scIGHYeKzT&format=png",
+        "10d": "https://img.icons8.com/?size=48&id=bFjyGgCI62A8&format=png",
+        "10n": "https://img.icons8.com/?size=48&id=bFjyGgCI62A8&format=png",
+        "11d": "https://img.icons8.com/?size=48&id=1MIo4L4pTj6s&format=png",
+        "11n": "https://img.icons8.com/?size=48&id=1MIo4L4pTj6s&format=png",
+        "13d": "https://img.icons8.com/?size=48&id=oYGjazh2cGfB&format=png",
+        "13n": "https://img.icons8.com/?size=48&id=oYGjazh2cGfB&format=png",
+        "50d": "https://img.icons8.com/?size=48&id=Uf8OFfff3BZZ&format=png",
+        "50n": "https://img.icons8.com/?size=48&id=Uf8OFfff3BZZ&format=png",
+      };
+      return translations[description] || description;
+    },
     validateInput() {
       if (!this.searchQuery) {
-        this.errorMessage = 'Please enter a city name.';
+        this.errorMessage = "Please enter a city name.";
         return false;
       }
       const cityNamePattern = /^[a-zA-Z\s-]+$/;
       if (!cityNamePattern.test(this.searchQuery)) {
-        this.errorMessage = 'Please enter a valid city name.';
+        this.errorMessage = "Please enter a valid city name.";
         return false;
       }
-      this.errorMessage = '';
+      this.errorMessage = "";
       return true;
     },
     async searchCity() {
@@ -95,39 +112,57 @@ export default {
 
       try {
         const API_KEY = import.meta.env.VITE_API_KEY;
-        const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(this.searchQuery)}&appid=${API_KEY}`;
+        const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(
+          this.searchQuery
+        )}&appid=${API_KEY}`;
         const response = await fetch(API_URL);
 
         if (!response.ok) {
-          throw new Error('City not found');
+          throw new Error("City not found");
         }
 
         const data = await response.json();
 
         if (data.cod !== 200) {
-          throw new Error('City not found');
+          throw new Error("City not found");
         }
 
         const iconCode = data.weather[0].icon;
-        const iconPath = `_nuxt/static/img/${iconCode}.png`;
 
         this.weatherData = {
           city: data.name,
-          temperature: (data.main.temp - 273.15).toFixed(2) + '°C',
+          temperature: (data.main.temp - 273.15).toFixed(2) + "°C",
           description: this.translateDescription(iconCode),
-          humidity: data.main.humidity + '%',
-          icon: iconPath,
+          humidity: data.main.humidity + "%",
+          icon: this.getIconUrl(iconCode),
           wind: data.wind.speed,
         };
 
         this.weatherVisible = true;
-        this.errorMessage = '';
+        this.errorMessage = "";
+
+        localStorage.setItem("weatherData", JSON.stringify(this.weatherData));
+        localStorage.setItem(
+          "weatherVisible",
+          JSON.stringify(this.weatherVisible)
+        );
       } catch (error) {
-        this.errorMessage = 'City not found. Please try again.';
+        this.errorMessage = "City not found. Please try again.";
         this.weatherData = null;
         this.weatherVisible = false;
       }
     },
+    loadWeatherData() {
+      const storedWeatherData = localStorage.getItem("weatherData");
+      const storedWeatherVisible = localStorage.getItem("weatherVisible");
+      if (storedWeatherData && storedWeatherVisible) {
+        this.weatherData = JSON.parse(storedWeatherData);
+        this.weatherVisible = JSON.parse(storedWeatherVisible);
+      }
+    },
+  },
+  mounted() {
+    this.loadWeatherData();
   },
 };
 </script>
--- components/weather.vue
+++ components/weather.vue
@@ -1,47 +1,26 @@
 <template>
   <div
-    class="border-black border-2 p-2.5 outline-none shadow-[5px_5px_0px_rgba(0,0,0,1)] shadow-[5px_5px_0px_rgba(0,0,0,1)] flex items-center justify-center text-3xl p-6 sm:p-7 md:p-15"
-  >
+    class="border-black border-2 p-2.5 outline-none shadow-[5px_5px_0px_rgba(0,0,0,1)] shadow-[5px_5px_0px_rgba(0,0,0,1)] flex items-center justify-center text-3xl p-6 sm:p-7 md:p-15">
     <div
-      class="city rounded-2xl outline-none w-full sm:w-11/12 p-2 flex justify-center items-center flex-col font-sawarabi"
-    >
+      class="city rounded-2xl outline-none w-full sm:w-11/12 p-2 flex justify-center items-center flex-col font-sawarabi">
       <h1
-        class="text-4xl sm:text-2xl md:text-5xl lg:text-5xl m-3 font-bold break-words flex items-center underline text-stone-800"
-      >
+        class="text-4xl sm:text-2xl md:text-5xl lg:text-5xl m-3 font-bold break-words flex items-center underline text-stone-800">
         {{ city }}
-        <img
-          src="https://img.icons8.com/?size=50&id=7880&format=png"
-          class="mx-2 w-7 sm:w-10 lg:w-10 md:w-17"
-        />
+        <img src="https://img.icons8.com/?size=50&id=7880&format=png" class="mx-2 w-7 sm:w-10 lg:w-10 md:w-17" />
       </h1>
-      <h1
-        class="font-light text-4xl sm:text-4xl md:text-5xl lg:text-6xl font-thin break-words flex items-center m-3"
-      >
+      <h1 class="font-light text-4xl sm:text-4xl md:text-5xl lg:text-6xl font-thin break-words flex items-center m-3">
         {{ temperature }}
-        <img
-          :src="icon"
-          class="w-20 sm:w-30 lg:w-35 md:w-45 mx-5"
-        />
+        <img :src="icon" class="w-20 sm:w-30 lg:w-35 md:w-45 mx-5" />
       </h1>
       <h2 class="text-base text-lg sm:text-md md:text-lg lg:text-xl my-1">
         {{ description }} <br />
       </h2>
       <div class="flex">
-        <h2
-          class="text-base sm:text-lg md:text-xl lg:text-2xl flex flex-col my-5 mr-20"
-        >
-          <img
-            src="https://img.icons8.com/?size=48&id=9ClbMTh8trmW&format=png"
-            class="w-10 sm:w-10"
-          />{{ humidity }}
+        <h2 class="text-base sm:text-lg md:text-xl lg:text-2xl flex flex-col my-5 mr-20">
+          <img src="https://img.icons8.com/?size=48&id=9ClbMTh8trmW&format=png" class="w-10 sm:w-10" />{{ humidity }}
         </h2>
-        <h2
-          class="text-base sm:text-lg md:text-xl lg:text-2xl flex flex-col my-5 ml-20"
-        >
-          <img
-            src="https://img.icons8.com/?size=48&id=RtDA8YDN9Mi9&format=png"
-            class="w-10 sm:w-10"
-          />{{ wind }}
+        <h2 class="text-base sm:text-lg md:text-xl lg:text-2xl flex flex-col my-5 ml-20">
+          <img src="https://img.icons8.com/?size=48&id=RtDA8YDN9Mi9&format=png" class="w-10 sm:w-10" />{{ wind }}
         </h2>
       </div>
     </div>
@@ -76,11 +55,5 @@ export default {
       required: true,
     },
   },
-  // methods: {
-  //   getWeatherIconUrl(iconCode) {
-  //     return `_nuxt/components/icons/${iconCode}.png`;
-  //     return `_nuxt/static/img/${iconCode}.png`;
-  //   },
-  // },
 };
 </script>
